ec39789b47ce448b9182ee36865501b41828583b
Simon RECHER <voileux@paulla.asso.fr>
2015-02-24 16:56:57 +0100

    merge of errors

--- bacula/snmpd/server.py
+++ bacula/snmpd/server.py
@@ -178,7 +178,7 @@ class SQLObject(object):
             return "1"
 
         for job in jobs:
-            if job['JobStatus'] != 'T':
+            if job['JobErrors'] == 1:
                 return "1"
 
         return "0"
